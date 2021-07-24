c539ba897809c543b0ca6c1b44e45f7677ff4183
C-atarina <catarina.silvaroriz@viacesi.fr>
2021-07-24 17:12:57 +0200

    implentation servomoteur

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
 #include <Keypad.h>
@@ -6,33 +7,33 @@
 #define RST_PIN 5
 #define DLENGTH 7
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+Servo myservo;	// Create Servo instance.
 
 // Function -------------
 
 /*
-	Il faudra suremnent utiliser un "StateDoor"
-	Pour savoir dans quelle état et la porte et ne 
-	pas trop tourner le servo moteur
+  Il faudra suremnent utiliser un "StateDoor"
+  Pour savoir dans quelle état et la porte et ne
+  pas trop tourner le servo moteur
 */
 
 void Open() {
-	Serial.println("Authorized access");
-	digitalWrite(11, HIGH);
-	delay(3000);
+  Serial.println("Authorized access");
+  digitalWrite(11, HIGH);
+  myservo.write(0); // Open servo moteur
+  delay(3000);
 
-	// Open servo moteur
-	
-	digitalWrite(11, LOW);
+
+  digitalWrite(11, LOW);
 }
 
 void Close() {
-	Serial.println("Access denied");
-	digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
-	delay(3000);
+  Serial.println("Access denied");
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  myservo.write(90); // Close servo moteur
+  delay(3000);
 
-	// Close servo moteur
-
-	digitalWrite(12, LOW);
+  digitalWrite(12, LOW);
 }
 
 // Code -----------------
@@ -41,10 +42,10 @@ const byte COLS = 4; //four columns
 
 //define the cymbols on the buttons of the keypads
 char hexaKeys[ROWS][COLS] = {
-	{'1','2','3','A'},
-	{'4','5','6','B'},
-	{'7','8','9','C'},
-	{'*','0','#','D'}
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
 };
 
 byte rowPins[ROWS] = {29, 28, 27, 26}; //connect to the row pinouts of the keypad
@@ -59,68 +60,71 @@ int index=0;
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 void code_porte() {
-	char customKey = customKeypad.getKey();
+  char customKey = customKeypad.getKey();
 
-	if (customKey) {
-		//Serial.println(customKey);
-		digits[index]=customKey;
-		index=index+1;
+  if (customKey) {
+    //Serial.println(customKey);
+    digits[index]=customKey;
+    index=index+1;
 
-		if(index==DLENGTH){
-			//Serial.println(digits);
-			index=0;
-				
-			if(strcmp(digits,code)==0){
-				Open();
-			} else {
-				Close();
-			}
-		}
-	}
+    if(index==DLENGTH){
+      //Serial.println(digits);
+      index=0;
+
+      if(strcmp(digits,code)==0){
+        Open();
+      } else {
+        Close();
+      }
+    }
+  }
 }
 
 void setup() {
-	Serial.begin(9600);   // Initiate a serial communication
-	SPI.begin();      // Initiate  SPI bus
-	mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
 
-	Serial.println("Approximate your card to the reader...");
-	Serial.println();
+  myservo.attach(9); // Pin du Servo
+  myservo.write(90); // Initiat servomotor position
 
-	pinMode(11, OUTPUT);
-	pinMode(12, OUTPUT);
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
+
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 void loop() {
-	// Check code
-	code_porte();
-	
-	// Look for new cards
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
-		return;
-	}
+  // Check code
+  code_porte();
 
-	// Select one of the cards
-	if ( ! mfrc522.PICC_ReadCardSerial()) {
-		return;
-	}
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
 
-	// Card ID
-	String content= "";
-	byte letter;
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
 
-	for (byte i = 0; i < mfrc522.uid.size; i++) {
-		content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-		content.concat(String(mfrc522.uid.uidByte[i], HEX));
-	}
+  // Card ID
+  String content= "";
+  byte letter;
 
-	Serial.println();
-	content.toUpperCase();
-	
-	if (content.substring(1) == "39 38 36 94") {
-		Open();
-	}
-	else if (content.substring(1) != "39 38 36 94") {
-		Close();
-	}
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+
+  Serial.println();
+  content.toUpperCase();
+
+  if (content.substring(1) == "39 38 36 94") {
+    Open();
+  }
+  else if (content.substring(1) != "39 38 36 94") {
+    Close();
+  }
 }
